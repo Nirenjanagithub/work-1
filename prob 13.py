4aea7b6824e5adc764756c685ee0432859c8e6d6
Nirenjana <unirenjana@gmail.com>
2023-12-13 19:09:33 +0530

    updated

--- prob 13.py
+++ prob 13.py
@@ -1,7 +1,14 @@
 arr = ['x','y','z']
 letter = input("Enter :")
-for letter in arr:
-    if letter == arr:
-        print(letter)
-else:
-    print(False)
+for i in arr:
+     if letter == i:
+         print(letter, " exists")
+         break
+     else:
+          print(letter, "Do not exists")
+          break
+
+        
+       
+
+    
