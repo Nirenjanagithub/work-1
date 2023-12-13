aebff7d1980078d8d103fb696416cd95c16d3c48
Nirenjana <unirenjana@gmail.com>
2023-12-13 10:31:43 +0530

    update prob 1

--- prob1.py
+++ prob1.py
@@ -1,2 +1,8 @@
-number=(" 1\n 2\n 3\n 4\n 5\n 6\n 7\n 8\n 9\n 10\n")
-print (number)
+#number=(" 1\n 2\n 3\n 4\n 5\n 6\n 7\n 8\n 9\n 10\n")#
+#print (number)#
+#or#
+i=1
+while i < 10:
+    print(i)
+    i += 1
+print("10")
