56aa54244d69d238286535aa752a29a9386b66b8
Nirenjana <unirenjana@gmail.com>
2023-12-14 16:23:15 +0530

    updated

--- prob 13.py
+++ prob 13.py
@@ -1,18 +1,15 @@
+from __future__ import print_function
 arr = ['x','y','z']
 letter = input("Enter :")
 for i in arr:
-     if letter == i:
-         print(letter, " exists")
-         break
-for j in arr:
-     if letter!= i:
-          print(letter, "Do  not exists")
-          break
-          
-          
-          
+    if letter == i:
+        print(letter, " exists")
+        break
          
-   
-    
+else:
+    print("No")
 
+             
     
+    
+   
